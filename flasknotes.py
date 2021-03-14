e6b99ec98c4540086a91a96b97c1073b6b540c04
fivmin <80613844+fivmin@users.noreply.github.com>
2021-03-14 18:53:19 +0530

    Update flasknotes.py

--- flasknotes.py
+++ flasknotes.py
@@ -60,8 +60,8 @@ def delete_entry(title):
 def add():
 	if request.method == 'POST':
 		note = dict(request.form)
-		today = str(datetime.now())
-		note['date_posted'] = today.strftime('%B %d,%Y %H:%:M:%S')
+		today = datetime.now().strftime('%B %d,%Y %H:%:M:%S')
+		note['date_posted'] = str(today)
 		add_note((note['title'],note['author'],note['date_posted']))
 	return render_template('add.html')	
 
